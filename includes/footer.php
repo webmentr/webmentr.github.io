1846e7d2219c56884423ee455a1b28ffc2627e38
webmentr <webmentr@gmail.com>
2026-01-31 01:00:05 +0600

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,41 @@
+<footer class="bg-dark text-light pt-5">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4 mb-4">
+                <h5>WebMentr</h5>
+                <p>
+                    Professional Web Design, Graphic Design & IT Solutions
+                    helping businesses grow digitally.
+                </p>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="about.html" class="text-light text-decoration-none">About</a></li>
+                    <li><a href="services.html" class="text-light text-decoration-none">Services</a></li>
+                    <li><a href="portfolio.html" class="text-light text-decoration-none">Portfolio</a></li>
+                    <li><a href="contact.html" class="text-light text-decoration-none">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5>Contact</h5>
+                <p><i class="fa fa-envelope"></i> info@webmentr.com</p>
+                <p><i class="fa fa-phone"></i> +880-XXXXXXXXXX</p>
+            </div>
+
+        </div>
+
+        <hr class="border-secondary">
+
+        <div class="text-center pb-3">
+            © 2026 WebMentr. All Rights Reserved.
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
