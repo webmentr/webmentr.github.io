e6cc17fab2a343fa5c2f48a45db2e4e3a39cb4e1
webmentr <webmentr@gmail.com>
2026-03-05 16:12:54 +0600

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,15 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
 
+document.addEventListener("DOMContentLoaded", function () {
 fetch("/includes/header.html")
 .then(response => response.text())
 .then(data => {
 document.getElementById("header").innerHTML = data;
 });
-
 fetch("/includes/footer.html")
 .then(response => response.text())
 .then(data => {
 document.getElementById("footer").innerHTML = data;
 });
-
+});
+const currentPage = location.pathname.split("/").pop();
+document.querySelectorAll(".nav-link").forEach(link => {
+if(link.getAttribute("href") === currentPage){
+link.classList.add("active");
+}
 });
